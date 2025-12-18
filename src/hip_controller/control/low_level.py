28abc60f40bd5f2785adced857f8c29c53ff51e7
tsmorz <tonysmoragiewicz@gmail.com>
2025-12-18 10:53:57 -0700

    rename s gait to gait speed

--- src/hip_controller/control/low_level.py
+++ src/hip_controller/control/low_level.py
@@ -12,16 +12,16 @@ def stop_condition(theta: float, theta_dot: float) -> bool:
     :param float theta_dot: Angle in radians / sec.
     :return: Whether the stop condition has been met.
     """
-    s_gait = get_s_gait(theta=theta, theta_dot=theta_dot)
+    gait_speed = get_gait_speed(theta=theta, theta_dot=theta_dot)
 
-    return s_gait < STOP_THRESHOLD
+    return gait_speed < STOP_THRESHOLD
 
 
-def get_s_gait(theta: float, theta_dot: float) -> float:
+def get_gait_speed(theta: float, theta_dot: float) -> float:
     """Calculate the s gait.
 
     :param float theta: angle in radians.
     :param float theta_dot: angle in radians / sec.
-    :returns: The s gait.
+    :returns: The gait speed.
     """
     return np.sqrt(theta**2 + theta_dot**2)
