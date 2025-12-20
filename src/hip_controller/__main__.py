5305e683f71a3cd29ee42a00d637077e4ae70c79
tsmorz <tonysmoragiewicz@gmail.com>
2025-12-20 08:38:55 -0700

    add main controller class

--- src/hip_controller/__main__.py
+++ src/hip_controller/__main__.py
@@ -2,13 +2,32 @@
 
 import argparse
 
-from hip_controller.app import main
+from hip_controller.app import WalkOnController
 from hip_controller.definitions import DEFAULT_LOG_LEVEL, LogLevel
+from hip_controller.utils import setup_logger
+
+
+def main(
+    log_level: str = DEFAULT_LOG_LEVEL, stderr_level: str = DEFAULT_LOG_LEVEL
+) -> None:  # pragma: no cover
+    """Run the main pipeline.
+
+    :param log_level: The log level to use.
+    :param stderr_level: The std err level to use.
+    :return: None
+    """
+    setup_logger(log_level=log_level, stderr_level=stderr_level)
+
+    controller = WalkOnController(freq=1.0)
+    while True:
+        controller.step()
+
 
 if __name__ == "__main__":  # pragma: no cover
     parser = argparse.ArgumentParser("Run the pipeline.")
     parser.add_argument(
         "--log-level",
+        "-l",
         default=DEFAULT_LOG_LEVEL,
         choices=list(LogLevel()),
         help="Set the log level.",
@@ -17,6 +36,7 @@ if __name__ == "__main__":  # pragma: no cover
     )
     parser.add_argument(
         "--stderr-level",
+        "-s",
         default=DEFAULT_LOG_LEVEL,
         choices=list(LogLevel()),
         help="Set the std err level.",
--- src/hip_controller/app.py
+++ src/hip_controller/app.py
@@ -1,19 +1,46 @@
 """Sample doc string."""
 
+import time
+
+import numpy as np
 from loguru import logger
 
-from hip_controller.definitions import DEFAULT_LOG_LEVEL
-from hip_controller.utils import setup_logger
+from hip_controller.control.low_level import get_gait_speed, stop_condition
 
 
-def main(
-    log_level: str = DEFAULT_LOG_LEVEL, stderr_level: str = DEFAULT_LOG_LEVEL
-) -> None:
-    """Run the main pipeline.
+class WalkOnController:
+    """Walk ON Controller for the lower limb exosuit."""
 
-    :param log_level: The log level to use.
-    :param stderr_level: The std err level to use.
-    :return: None
-    """
-    setup_logger(log_level=log_level, stderr_level=stderr_level)
-    logger.info("Hello, world!")
+    def __init__(self, freq: float):
+        """Initialize the controller.
+
+        :param freq: Frequency of the controller.
+        :return: None
+        """
+        self.freq = freq
+
+    def step(self):
+        """Step the controller ahead."""
+        logger.debug("Stepping controller ahead.")
+
+        # High-level
+
+        # Mid-level
+
+        # Low-level
+        try:
+            theta, theta_dot = self.get_sensor_data()
+            gait_speed = get_gait_speed(theta=theta, theta_dot=theta_dot)
+            if stop_condition(gait_speed=gait_speed):
+                logger.info("Stop condition reached.")
+                return
+        except Exception as err:
+            logger.error(f"{err} - Something went wrong.")
+
+        time.sleep(1 / self.freq)
+
+    @staticmethod
+    def get_sensor_data() -> tuple[float, float]:
+        """Get fake sensor data."""
+        logger.debug("Getting fake sensor data.")
+        return np.sin(time.monotonic() / 2), np.cos(time.monotonic() / 2)
